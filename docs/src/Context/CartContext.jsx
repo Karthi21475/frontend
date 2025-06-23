b0e8e765283f757087e5b1ab792aa0edbae1b8ef
Karthik <Karthigaming121@gamil.com>
2025-06-23 20:28:55 +0530

    testing Loader-test4

--- docs/src/Context/CartContext.jsx
+++ docs/src/Context/CartContext.jsx
@@ -17,33 +17,26 @@ export const CartProvider=({children})=>{
     }
     
     const AddcartItem=async(productDetails)=>{
-        setLoader(true)
         const res=await axios.post(`${import.meta.env.VITE_API_URL}`+'/api/cart',productDetails,{headers:{'Content-Type':'application/json'},withCredentials: true});
         await getCartItems();
-        setLoader(false)
         
         if (res.data.message==="Item Added to Cart"){
             console.log("added it to the cart");
         }
     }
     const UpdatecartItem=async(id,quantity)=>{
-        
-        setLoader(true)
         const Data={quantity}
         
         const res=await axios.put(`${import.meta.env.VITE_API_URL}`+`/api/cart/${id}`,Data,{headers:{'Content-Type':'application/json'},withCredentials: true});
         await getCartItems();
-        setLoader(false)
         
         if (res.data.message==="Item Updated in Cart"){
             console.log("Updated item in the cart");
         }
     }
     const DeletecartItem=async(id)=>{
-        setLoader(true)
         await axios.delete(`${import.meta.env.VITE_API_URL}/api/cart/${id}`,{withCredentials: true});
         await getCartItems();
-        setLoader(false)
     }
 
     useEffect(()=>{
