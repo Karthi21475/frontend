5bbebe308a8f25ac0f6c8d3ef0bc8e831271e5ca
Karthik <Karthigaming121@gamil.com>
2026-03-17 23:51:19 +0530

    made the ui responsive

--- docs/src/components/Nav.jsx
+++ docs/src/components/Nav.jsx
@@ -5,13 +5,13 @@ import axios from 'axios';
 import '../styles/Nav.css';
 import { useNavigate } from 'react-router-dom';
 function Nav() {
+    const [show,setShow]=useState(false);
     const [token,setToken]=useState(false);
     const [admin,setAdmin]=useState(false);
     const navigate=useNavigate();
     useEffect(()=>{
         const authchecker=async()=>{
             const res=await axios.get(`${import.meta.env.VITE_API_URL}`+'/api/user/auth',{headers:{'Content-Type':'application/json'},withCredentials: true})
-            
             
             if(res.data.message==="User Authenticated"){
                 setToken(true);
@@ -34,17 +34,23 @@ function Nav() {
     <>
         <nav className="nav-container">
             <h1 className="logo">VLN</h1>
-            <ul className="nav-links">
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/products/?limit=9&page=1'>Products</Link></li>
-                <li><Link to='/cart'>Cart</Link></li>
-                {admin && <li><Link to='/add-product'>Add Product</Link></li>}
-            </ul>
-            {token?
-            <button className="btn1" onClick={()=>handleClick()}>Logout</button>
-            :
-            <Link to='/login' className="btn1">Login</Link>
-            }
+            <div className='ham min-[750px]:hidden' onClick={()=>setShow(prev=>!prev)}><i className='bx bx-menu'></i></div>
+            <div className={`drop-down min-[750px]:flex ${show?"max-[750px]:flex":"max-[750px]:hidden"}`}>
+                <div className='ms-auto min-[750px]:hidden'>
+                    <i className='bx bx-x' onClick={()=>setShow(prev=>!prev)}></i>
+                </div>
+                <ul className="nav-links">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/products/?limit=9&page=1'>Products</Link></li>
+                    <li><Link to='/cart'>Cart</Link></li>
+                    {admin && <li><Link to='/add-product'>Add Product</Link></li>}
+                </ul>
+                {token?
+                    <button className="btn1" onClick={()=>handleClick()}>Logout</button>
+                    :
+                    <Link to='/login' className="btn1">Login</Link>
+                }
+            </div>
         </nav>
     </>
     )
